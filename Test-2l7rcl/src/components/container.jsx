9d7ddf79dca9f9f08c357646d813f3e7bfab853a
Devin Altobello <dl.altobello@gmail.com>
2025-04-10 14:40:52 -0400

    Remove starter code

--- Test-2l7rcl/src/components/container.jsx
+++ Test-2l7rcl/src/components/container.jsx
@@ -1,36 +1,19 @@
-import React, { useState } from "react";
-import { Content } from "./content";
+import React from "react";
 import { Footer } from "./footer";
 import { Header } from "./header";
 import { FileProcessor } from "./fileProcessor";
+import "./container.css"
 
 export const Container = () => {
-  const [message, setMessage] = useState([]);
-
-  const writeToConsole = (consoleMessage) => {
-    setMessage((prevMessage) => [...prevMessage, consoleMessage]);
-  };
-
-  const clearConsole = () => {
-    setMessage([]);
-  };
 
   return (
     <>
       <div className="plugin-container">
         <Header />
+        <div className="wrapper"></div>
         <FileProcessor />
-        <Content message={message} />
-        <Footer writeToConsole={writeToConsole} clearConsole={clearConsole} />
+        <Footer />
       </div>
-      <style>
-        {`
-    .plugin-container {
-  color: white;
-  padding: 0 16px;
-  }
-    `}
-      </style>
     </>
   );
 };
