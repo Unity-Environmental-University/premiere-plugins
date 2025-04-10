6c8b4181341c43d58e0dbad14104279a503bd90e
Devin Altobello <dl.altobello@gmail.com>
2025-04-10 14:46:40 -0400

    Update heading text

--- Test-2l7rcl/src/components/header.jsx
+++ Test-2l7rcl/src/components/header.jsx
@@ -5,7 +5,7 @@ import "./header.css";
 export const Header = () => {
   return (
     <sp-body>
-      <div className="plugin-header">Convert SRT to Text</div>
+      <div className="plugin-header">Convert Exported SRT to Text</div>
     </sp-body>
   );
 };
