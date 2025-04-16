b46c35cf2b3e502d353d8e54d96044caf522fda8
Devin Altobello <dl.altobello@gmail.com>
2025-04-16 13:58:50 -0400

    Add isFileProcessed state and pass to FileProcessor, Footer

--- Test-2l7rcl/src/components/container.jsx
+++ Test-2l7rcl/src/components/container.jsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Footer } from "./footer";
 import { Header } from "./header";
 import { FileProcessor } from "./fileProcessor";
 import "./container.css"
 
 export const Container = () => {
+  const [isFileProcessed, setIsFileProcessed] = useState(false)
+
+  useEffect(() => {
+    console.log("File Processed Status:", isFileProcessed);
+  }, [isFileProcessed]);
 
   return (
     <>
       <div className="plugin-container">
         <Header />
         <div className="wrapper"></div>
-        <FileProcessor />
-        <Footer />
+        <FileProcessor setIsFileProcessed={setIsFileProcessed}/>
+        <Footer isFileProcessed={isFileProcessed}/>
       </div>
     </>
   );
