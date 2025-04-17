163ce3cec409f2ae5905bc9d620ae5b667c12013
Devin Altobello <dl.altobello@gmail.com>
2025-04-17 15:17:10 -0400

    Refactor Footer to StatusComplete with checkmark svg

---
+++ Test-2l7rcl/src/components/statusComplete.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./statusComplete.css";
+import { CheckmarkCircleIcon } from "./checkMarkCircle";
+
+export const StatusComplete = () => {
+  return (
+      <div className="status-complete-container">
+         <sp-body>Complete</sp-body>
+        <div className="icon-container">
+          <CheckmarkCircleIcon fillColor="#008729" className="checkmark"/>
+        </div>
+      </div>
+  );
+};
