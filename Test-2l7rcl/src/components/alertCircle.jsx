58e33bf589708da0a00b8be47c0597ed4ebc94c6
Devin Altobello <130494366+alto-fx@users.noreply.github.com>
2025-04-18 13:58:29 -0400

    Add AlertCircleIcon component

--- Test-2l7rcl/src/components/alertCircle.jsx
+++ Test-2l7rcl/src/components/alertCircle.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const AlertIcon = ({ fillColor }) => (
+export const AlertCircleIcon = ({ fillColor }) => (
   <svg xmlns="http://www.w3.org/2000/svg" height="18" viewBox="0 0 18 18" width="18">
     <title>Alert Circle</title>
     <rect id="Canvas" fill="none" opacity="0" width="18" height="18" />
