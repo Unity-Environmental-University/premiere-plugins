4bc187213fd9f7af187b8306db0b8a886fef9341
Devin Altobello <dl.altobello@gmail.com>
2025-04-24 13:25:48 -0400

    Update className

--- Test-2l7rcl/src/components/statusComplete.jsx
+++ Test-2l7rcl/src/components/statusComplete.jsx
@@ -4,7 +4,7 @@ import { CheckmarkCircleIcon } from "./checkMarkCircle";
 
 export const StatusComplete = () => {
   return (
-      <div className="status-complete-container">
+      <div className="status-container">
          <sp-body>Complete</sp-body>
         <div className="icon-container">
           <CheckmarkCircleIcon fillColor="#008729" className="checkmark"/>
